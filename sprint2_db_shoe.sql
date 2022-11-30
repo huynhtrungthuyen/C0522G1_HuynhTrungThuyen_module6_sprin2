6fdfa7c980b216dff5a13b9ec74d6078502d2be4
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-11-30 16:15:51 +0700

    2

--- sprint2_db_shoe.sql
+++ sprint2_db_shoe.sql
@@ -1,6 +1,6 @@
-create database if not exists project_module_6_sprin2;
+create database if not exists project_module_6_sprint2;
 
-use project_module_6_sprin2;
+use project_module_6_sprint2;
 
 create table if not exists user(
 	username varchar(30) primary key,
@@ -38,7 +38,7 @@ create table if not exists shoe_type(
 create table if not exists shoe(
 	id int primary key auto_increment,
     name varchar(100),
-    prime int,
+    price int,
     discount int,
     manufacturer varchar(50),
     image text,
@@ -47,20 +47,6 @@ create table if not exists shoe(
     shoe_type_id int,
     foreign key(shoe_size_id) references shoe_size(id),
     foreign key(shoe_type_id) references shoe_type(id)
-);
-
-create table if not exists customer(
-	id int primary key auto_increment,
-	name varchar(30),
-	is_delete bit default 0,
-	day_of_birth varchar(30),
-	gender int,
-	id_card varchar(12),
-	email varchar(100),
-	address varchar(200),
-	phone_number varchar(15),
-	username varchar(30) unique,
-	foreign key (username) references user(username)
 );
 
 create table if not exists customer(
